2adac443df9d1795f377a2ef65fbbbdd59f20511
MusicMel0n <78413704+MusicMel0n@users.noreply.github.com>
2021-11-23 16:08:18 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -5,16 +5,33 @@ from kivy.app import App
 from kivy.uix.button import Label
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.textinput import TextInput
+
+Builder.load_file('adogforyou.kv')
 
 Window.size = (428, 926)
 
-class CustomWidget(Widget):
+class ScreenOne(Screen):
     pass
 
-class adogforyouapp(App):
+class ScreenTwo(Screen):
+    pass
+
+class ScreenThree(Screen):
+    pass
+
+screen_manager =  ScreenManager()
+
+screen_manager.add_widget(ScreenOne(name='screen_one'))
+screen_manager.add_widget(ScreenTwo(name='screen_two'))
+screen_manager.add_widget(ScreenThree(name='screen_three'))
+
+class adogforyouApp(App):
     def build(self):
-        return CustomWidget()
+        return screen_manager
 
-dogforyou = adogforyouapp()
+app = adogforyouApp()
 
-dogforyou.run()
+app.run()
